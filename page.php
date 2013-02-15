409b56b6aacc186b1351dcdcf0c3aeca48c1738a
sourabhgarg01 <sourabh.asct@gmail.com>
2013-02-15 01:43:00 -0800

    Create page.php

---
+++ page.php
@@ -0,0 +1,8 @@
+<?php 
+/*
+Now that we have the function, nothing simpler:
+We just have to edit the file where we want our breadcrumb to appear 
+I think that archive.php, single.php and page.phpis a good choice â€“ and call the function.
+*/
+the_breadcrumb();
+?>
